316ab69267963f721bb60f7005f300bf70b2e9b0
Alexandru Damian <70851317+Desu42@users.noreply.github.com>
2021-04-07 09:35:55 +0300

    test

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+We can use https://pypi.org/project/webdriver-manager/ library and setup driver
+dependency programmatically at run time.
+CONTL - Continental, name of the hotel
+TC - test case
+"""
+
+__author__ = "Alexandru Damian"
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('QA Tests')
