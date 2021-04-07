bcbd55e168ade47b77f80577b8610a890bf93170
Alexandru Damian <70851317+Desu42@users.noreply.github.com>
2021-04-07 20:51:47 +0300

    update

--- CONTL_Website_Tests/001_CONTL_Website_PageNav_TC.py
+++ CONTL_Website_Tests/001_CONTL_Website_PageNav_TC.py
@@ -1,22 +1,14 @@
 import allure
 from allure_commons.types import AttachmentType
 from selenium import webdriver
+# from selenium.webdriver import ActionChains
 
 
+# The images are huge. It takes a very long time to load the pages.
 @allure.severity(allure.severity_level.NORMAL)
 class TestContinentalHomePage:
 
     base_url = "https://alexandrularion.github.io/hotel-continental/"
-
-    @allure.severity(allure.severity_level.BLOCKER)
-    def test_load_home_page(self):
-        self.driver = webdriver.Chrome()
-        driver = self.driver
-        self.driver.maximize_window()
-        driver.get(self.base_url)
-
-        assert "Continental" in driver.title
-        driver.close()
 
     @allure.severity(allure.severity_level.MINOR)
     def test_logo(self):
@@ -28,33 +20,161 @@ class TestContinentalHomePage:
         assert status
         driver.close()
 
-    @allure.severity(allure.severity_level.CRITICAL)
-    def test_sign_in(self):
+    @allure.severity(allure.severity_level.BLOCKER)
+    def test_load_home_page(self):
         self.driver = webdriver.Chrome()
         driver = self.driver
         self.driver.maximize_window()
         driver.get(self.base_url)
 
-        btn_sign_in = driver.find_element_by_xpath("//*[@id='header']/div[1]/div[2]/div/a[3]")
-        btn_sign_in.click()
+        assert "Continental" in driver.title
+        driver.close()
+    
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_home_page(self):
+        self.driver = webdriver.Chrome()
+        driver = self.driver
+        self.driver.maximize_window()
+        driver.get(self.base_url)
 
-        email = driver.find_element_by_id("email")
-        email.send_keys("example@gmail.com")
+        home_page_button = driver.find_element_by_class_name("home")
+        home_page_button.click()
 
-        psw = driver.find_element_by_id("psw")
-        psw.send_keys("example_password")
+        home_page_button_url = "https://alexandrularion.github.io/hotel-continental/index.html"
 
-        sign_in = driver.find_element_by_class_name("registerbtn")
-        sign_in.click()
-
-        title = self.driver.title
-
-        if title == "Welcome":
+        if home_page_button_url == driver.current_url:
             driver.close()
             assert True
-
         else:
-            allure.attach(self.driver.get_screenshot_as_png(), name="Login_Test",
+            allure.attach(self.driver.get_screenshot_as_png(), name="Navigation_Test",
+                          attachment_type=AttachmentType.PNG)
+            driver.close()
+            assert False
+    
+    @allure.severity(allure.severity_level.BLOCKER)
+    def test_rooms_page(self):
+        self.driver = webdriver.Chrome()
+        driver = self.driver
+        self.driver.maximize_window()
+        driver.get(self.base_url)
+
+        rooms_page_button = driver.find_element_by_class_name("rooms")
+        rooms_page_button.click()
+
+        rooms_page_button_url = "https://alexandrularion.github.io/hotel-continental/rentroom.html"
+
+        if rooms_page_button_url == driver.current_url:
+            driver.close()
+            assert True
+        else:
+            allure.attach(self.driver.get_screenshot_as_png(), name="Navigation_Test",
+                          attachment_type=AttachmentType.PNG)
+            driver.close()
+            assert False
+    
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_services_page(self):
+        self.driver = webdriver.Chrome()
+        driver = self.driver
+        self.driver.maximize_window()
+        driver.get(self.base_url)
+
+        services_page_button = driver.find_element_by_class_name("services")
+        services_page_button.click()
+
+        services_page_button_url = "https://alexandrularion.github.io/hotel-continental/services.html"
+
+        if services_page_button_url == driver.current_url:
+            driver.close()
+            assert True
+        else:
+            allure.attach(self.driver.get_screenshot_as_png(), name="Navigation_Test",
+                          attachment_type=AttachmentType.PNG)
+            driver.close()
+            assert False
+    
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_about_us_page(self):
+        self.driver = webdriver.Chrome()
+        driver = self.driver
+        self.driver.maximize_window()
+        driver.get(self.base_url)
+
+        about_page_button = driver.find_element_by_class_name("about")
+        about_page_button.click()
+
+        about_page_button_url = "https://alexandrularion.github.io/hotel-continental/about.html"
+
+        if about_page_button_url == driver.current_url:
+            driver.close()
+            assert True
+        else:
+            allure.attach(self.driver.get_screenshot_as_png(), name="Navigation_Test",
+                          attachment_type=AttachmentType.PNG)
+            driver.close()
+            assert False
+    
+    @allure.severity(allure.severity_level.BLOCKER)
+    def test_contact_page(self):
+        self.driver = webdriver.Chrome()
+        driver = self.driver
+        self.driver.maximize_window()
+        driver.get(self.base_url)
+
+        contact_page_button = driver.find_element_by_class_name("contact")
+        contact_page_button.click()
+
+        contact_page_button_url = "https://alexandrularion.github.io/hotel-continental/contact.html"
+
+        if contact_page_button_url == driver.current_url:
+            driver.close()
+            assert True
+        else:
+            allure.attach(self.driver.get_screenshot_as_png(), name="Navigation_Test",
+                          attachment_type=AttachmentType.PNG)
+            driver.close()
+            assert False
+    
+    # Book now is the same page as rent room
+    @allure.severity(allure.severity_level.BLOCKER)
+    def test_book_now_page(self):
+        self.driver = webdriver.Chrome()
+        driver = self.driver
+        self.driver.maximize_window()
+        driver.get(self.base_url)
+
+        book_page_button = driver.find_element_by_class_name("book_button")
+        book_page_button.click()
+
+        book_page_button_url = "https://alexandrularion.github.io/hotel-continental/rentroom.html"
+
+        if book_page_button_url == driver.current_url:
+            driver.close()
+            assert True
+        else:
+            allure.attach(self.driver.get_screenshot_as_png(), name="Navigation_Test",
+                          attachment_type=AttachmentType.PNG)
+            driver.close()
+            assert False
+    
+    # As of now, clicking on either Payment options or T&C takes you to the same page
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_information_page(self):
+        self.driver = webdriver.Chrome()
+        driver = self.driver
+        self.driver.maximize_window()
+        driver.get(self.base_url)
+
+        payment_page_button = driver.find_element_by_class_name("options")
+        payment_page_button.click()
+
+        payment_page_button_url = "https://alexandrularion.github.io/hotel-continental/information.html"
+
+        if payment_page_button_url == driver.current_url:
+            driver.close()
+            assert True
+        else:
+            allure.attach(self.driver.get_screenshot_as_png(), name="Navigation_Test",
                           attachment_type=AttachmentType.PNG)
             driver.close()
             assert False
---
+++ CONTL_Website_Tests/003_CONTL_Website_Login_TC.py
@@ -0,0 +1,39 @@
+import allure
+from allure_commons.types import AttachmentType
+from selenium import webdriver
+
+
+class TestContinentalWebsiteLogin:
+
+    base_url = "https://alexandrularion.github.io/hotel-continental/"
+
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_sign_in(self):
+        self.driver = webdriver.Chrome()
+        driver = self.driver
+        self.driver.maximize_window()
+        driver.get(self.base_url)
+
+        btn_sign_in = driver.find_element_by_xpath("//*[@id='header']/div[1]/div[2]/div/a[3]")
+        btn_sign_in.click()
+
+        email = driver.find_element_by_id("email")
+        email.send_keys("example@gmail.com")
+
+        psw = driver.find_element_by_id("psw")
+        psw.send_keys("example_password")
+
+        sign_in = driver.find_element_by_class_name("registerbtn")
+        sign_in.click()
+
+        title = self.driver.title
+
+        if title == "Welcome":
+            driver.close()
+            assert True
+
+        else:
+            allure.attach(self.driver.get_screenshot_as_png(), name="Login_Test",
+                          attachment_type=AttachmentType.PNG)
+            driver.close()
+            assert False
